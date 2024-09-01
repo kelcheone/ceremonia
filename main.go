5e65d2e6f5b82575dd070331f8a13ea10ac63290
kelche <kelcheone@gmail.com>
2024-09-01 09:48:30 +0300

    fix: versioninng only v1

--- main.go
+++ main.go
@@ -1,42 +1,12 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
-
-	dkgHandler "github.com/kelcheone/ceremonia/routes/dkg"
+	dockerServer "github.com/kelcheone/ceremonia/servers/docker"
 )
 
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/api/run-dkg", func(w http.ResponseWriter, r *http.Request) {
-		handler := &dkgHandler.DKGHandler{}
-		handler.RunDKGHandler(w, r)
-	}).Methods("POST")
-
-	r.HandleFunc("/api/get-file/{sessionId}", func(w http.ResponseWriter, r *http.Request) {
-		handler := &dkgHandler.DKGHandler{}
-		handler.ServeGeneratedFiles(w, r)
-	}).Methods("GET")
-
-	// health check
-	r.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	}).Methods("GET")
-
 	port := ":8090"
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-	})
+	newDockerServer := dockerServer.DockerSerVer{}
 
-	gHandler := c.Handler(r)
-
-	log.Printf("Running server on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, gHandler))
+	newDockerServer.Start(port)
 }
--- ui/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["media-v2.ssv.network"],
-  },
-};
-
-export default nextConfig;
