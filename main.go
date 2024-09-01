debecd34c2b9030d6e9f3d696369f63a2dba8a9c
kelche <kelcheone@gmail.com>
2024-09-01 10:00:49 +0300

    fix: versioninng to only use the docker run command

--- main.go
+++ main.go
@@ -1,42 +1,8 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gorilla/mux"
-	"github.com/rs/cors"
-
-	dkgHandler "github.com/kelcheone/ceremonia/routes/dkg"
-)
+import dockerServer "github.com/kelcheone/ceremonia/servers/docker"
 
 func main() {
-	r := mux.NewRouter()
-
-	r.HandleFunc("/api/run-dkg", func(w http.ResponseWriter, r *http.Request) {
-		handler := &dkgHandler.DKGHandler{}
-		handler.RunDKGHandler(w, r)
-	}).Methods("POST")
-
-	r.HandleFunc("/api/get-file/{sessionId}", func(w http.ResponseWriter, r *http.Request) {
-		handler := &dkgHandler.DKGHandler{}
-		handler.ServeGeneratedFiles(w, r)
-	}).Methods("GET")
-
-	// health check
-	r.HandleFunc("/api/health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	}).Methods("GET")
-
-	port := ":8090"
-	c := cors.New(cors.Options{
-		AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-	})
-
-	gHandler := c.Handler(r)
-
-	log.Printf("Running server on port %s\n", port)
-	log.Fatal(http.ListenAndServe(port, gHandler))
+	newServer := dockerServer.DockerSerVer{}
+	newServer.Start("8090")
 }
