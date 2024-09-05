f6669022279fb40480e01e79f9ee3869f2b221f4
kelche <kelcheone@gmail.com>
2024-09-05 14:24:14 +0300

    feat: app's type declarations

---
+++ ui/desktop/src/types/types.ts
@@ -0,0 +1,61 @@
+export type OperatorInfo = {
+  id: number;
+  public_key: string;
+  ip: string;
+};
+
+export type RequestValues = {
+  validators: number;
+  operatorIds: number[];
+  operatorsInfo: OperatorInfo[];
+  ownerAddr: string;
+  nonce: number;
+  withdrawAddr: string;
+  network: string;
+  expiry: number;
+};
+
+export type GenerateKeysForm = {
+  validators: number;
+  ownerAddress: string;
+  withdrawalAddress: string;
+  expiryTime: number;
+};
+
+export interface Operator {
+  id: number;
+  name: string;
+  type: string;
+  validators_count: number;
+  public_key: string;
+  performance: {
+    '30d': number;
+  };
+  fee: string;
+  is_valid: boolean;
+  dkg_address: string;
+  logo: string | undefined;
+  is_private: boolean;
+}
+
+export type GenerateKeysResponse = {
+  file: {
+    name: string;
+    url: string;
+  };
+  sessionId: string;
+  date: string;
+  expiration: string;
+  selectedOperators: Operator[];
+  message: string;
+};
+
+export type Chain = {
+  name: string;
+  id: number;
+};
+
+export const chains: Chain[] = [
+  { name: 'Ethereum', id: 1 },
+  { name: 'Holesky', id: 17000 }
+];
